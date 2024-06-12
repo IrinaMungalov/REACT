10b028ab2ba5be1ec738af3ae62fc898b5dec902
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2024-06-12 14:29:15 +0300

    Add files via upload

---
+++ react-flakes/src/flake/Flake.js
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+import "./Flake.css"
+
+
+const Flake = ({left}) => {
+
+    let [top, setTop] = useState(0)
+
+    const style = {
+        top: `${top}%`,
+        left: `${left}%`
+    }
+
+    return (
+        <>
+            <div 
+                className="flake"
+                style={style}            
+            ></div>
+            <button onClick={() => {
+                setTop(top+5)
+            }}>DOWN</button>
+        </>
+    )
+}
+
+export{Flake}
---
+++ react-flakes/src/scene/Scene.js
@@ -0,0 +1,15 @@
+import './Scene.css';
+
+import {Flake} from '../flake/Flake'
+import { randomCoordinate } from "../utils/coords"
+
+function Scene() {
+  return (
+    <div className="scene">
+      <Flake left={randomCoordinate()}/>
+      
+    </div>
+  );
+}
+
+export default Scene;
