a99796defe609aa889259cbe9d7e402c99462259
Irina <131948761+IrinaMungalov@users.noreply.github.com>
2024-07-03 13:43:38 +0300

    Add files via upload

---
+++ restaurant_new/db/dml.sql
@@ -0,0 +1,22 @@
+INSERT INTO products VALUES
+(
+    101, 
+    'pizza', 
+    'https://rabotaryadom.info/wp-content/cache/thumb/43/5c3293a19b8ac43_350x0.jpg',
+    100,
+    'MDL'
+),
+(
+    102, 
+    'risotto', 
+    'https://img.over-blog-kiwi.com/0/65/19/23/20191209/ob_02d697_risotto-parmesan-chorizo.jpg',
+    25,
+    'MDL'
+),
+(
+    103, 
+    'lasagna', 
+    'https://i.pinimg.com/originals/e6/83/4e/e6834ea984f5f066cddc7730137c2eab.jpg',
+    50,
+    'MDL'
+);
